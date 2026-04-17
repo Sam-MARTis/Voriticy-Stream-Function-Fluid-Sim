e8e251d810cd786bd8708fb74e9ebee4db3c78c7
Sam-MARTis <samanthmartis@gmail.com>
2026-04-17 20:37:45 +0530

    Fixed and added vorticity interpolation

---
+++ aux.cpp
@@ -0,0 +1,53 @@
+#include "constants.hpp"
+
+void print(const auto& message) {
+    std::cout << message << std::endl;
+}
+
+void find_velocity_at_point(float& u_x, float& u_y, const float px, const float py, const float* u, const float u0, const int nx, const int ny, const float dx, const float dy) {
+    const int i = static_cast<int>(px / dx);
+    const int j = static_cast<int>(py / dy);
+    if(i < 0 || i >= nx-1 || j < 0) {
+        u_x = u0 * (j >= ny-1); // top boundary has velocity u0, others have velocity 0
+        u_y = 0.0f;
+        return;
+    }
+    const float i_frac = (px - i * dx) / dx;
+    const float j_frac = (py - j * dy) / dy;
+    float ux[4] = {0, 0, 0, 0}; // bottom left, top left, top right, bottom right
+    float uy[4] = {0, 0, 0, 0};
+    ux[0] = u[2*(j*nx + i)];
+    uy[0] = u[2*(j*nx + i) + 1];
+
+    ux[1] = u[2*((j+1)*nx + i)];
+    uy[1] = u[2*((j+1)*nx + i) + 1];
+
+    ux[2] = u[2*((j+1)*nx + (i+1))];
+    uy[2] = u[2*((j+1)*nx + (i+1)) + 1];
+
+    ux[3] = u[2*(j*nx + (i+1))];
+    uy[3] = u[2*(j*nx + (i+1)) + 1];
+
+
+    u_x = (ux[0] * (1-i_frac) + ux[3] * i_frac) * (1-j_frac) + (ux[1] * (1-i_frac) + ux[2] * i_frac) * j_frac;
+    u_y = (uy[0] * (1-i_frac) + uy[3] * i_frac) * (1-j_frac) + (uy[1] * (1-i_frac) + uy[2] * i_frac) * j_frac;
+}
+
+void find_vorticity_at_point(float& ω_val, const float px, const float py, const float* ω, const int nx, const int ny, const float dx, const float dy) {
+    const int i = static_cast<int>(px / dx);
+    const int j = static_cast<int>(py / dy);
+    const float i_frac = (px - i * dx) / dx;
+    const float j_frac = (py - j * dy) / dy;
+    float ω_local[4] = {0, 0, 0, 0}; // bottom left, top left, top right, bottom right
+    if((i < 0 && (1-i_frac) > EPSILON1) || (i >= nx-1 && i_frac > EPSILON1) || (j < 0 && (1-j_frac) > EPSILON1) || (j >= ny-1 && j_frac > EPSILON1)) {
+        ω_val = 0.0f;
+        return;
+    }
+    ω_local[0] = ω[j*nx + i];
+
+    ω_local[1] = ω[(j+1)*nx + i];
+
+    ω_local[2] = ω[(j+1)*nx + (i+1)];
+    ω_local[3] = ω[j*nx + (i+1)];
+    ω_val = (ω_local[0] * (1-i_frac) + ω_local[3] * i_frac) * (1-j_frac) + (ω_local[1] * (1-i_frac) + ω_local[2] * i_frac) * j_frac;
+}
--- aux.hpp
+++ aux.hpp
@@ -1,35 +1,3 @@
-void print(const auto& message) {
-    std::cout << message << std::endl;
-}
-
-void find_velocity_at_point(float& u_x, float& u_y, const float px, const float py, const float* u, const int nx, const int ny, const float dx, const float dy) {
-    const int i = static_cast<int>(px / dx);
-    const int j = static_cast<int>(py / dy);
-    const float i_frac = (px - i * dx) / dx;
-    const float j_frac = (py - j * dy) / dy;
-    float ux[4] = {0, 0, 0, 0}; // bottom left, top left, top right, bottom right
-    float uy[4] = {0, 0, 0, 0};
-    const bool bottom_left_in_bound = (i >= 0 && i < nx-1) && (j >= 0 && j < ny-1);
-    const bool bottom_right_in_bound = (i >= -1 && i < nx-2) && (j >= 0 && j < ny-1);
-    const bool top_left_in_bound = (i >= 0 && i < nx-1) && (j >= -1 && j < ny-2);
-    const bool top_right_in_bound = (i >= -1 && i < nx-2) && (j >= -1 && j < ny-2);
-    const bool bounds[4] = {bottom_left_in_bound, top_left_in_bound, top_right_in_bound, bottom_right_in_bound};
-    if (bounds[0]) {
-        ux[0] = u[2*(j*nx + i)];
-        uy[0] = u[2*(j*nx + i) + 1];
-    }
-    if (bounds[1]) {
-        ux[1] = u[2*((j+1)*nx + i)];
-        uy[1] = u[2*((j+1)*nx + i) + 1];
-    }
-    if (bounds[2]) {
-        ux[2] = u[2*((j+1)*nx + (i+1))];
-        uy[2] = u[2*((j+1)*nx + (i+1)) + 1];
-    }
-    if (bounds[3]) {
-        ux[3] = u[2*(j*nx + (i+1))];
-        uy[3] = u[2*(j*nx + (i+1)) + 1];
-    }
-    u_x = (ux[0] * (1-i_frac) + ux[3] * i_frac) * (1-j_frac) + (ux[1] * (1-i_frac) + ux[2] * i_frac) * j_frac;
-    u_y = (uy[0] * (1-i_frac) + uy[3] * i_frac) * (1-j_frac) + (uy[1] * (1-i_frac) + uy[2] * i_frac) * j_frac;
-}
+void print(const auto& message);
+void find_velocity_at_point(float& u_x, float& u_y, const float px, const float py, const float* u, const float u0, const int nx, const int ny, const float dx, const float dy);
+void find_vorticity_at_point(float& ω_val, const float px, const float py, const float* ω, const int nx, const int ny, const float dx, const float dy);
